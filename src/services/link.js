4417a9ee044c6062a7f1d0dcac0cd3e91da6c101
Maity54321 <maitysurodoy@gmail.com>
2025-06-14 03:47:08 +0530

    Change link

--- src/services/link.js
+++ src/services/link.js
@@ -1,3 +1,3 @@
 console.log(process.env.REACT_APP_MYENV)
-exports.APIUrl = process.env.REACT_APP_MYENV === "prod" ?  "https://ecom-api-zdk9.onrender.com" : "http://localhost:4000";
-// exports.devLink = "http://192.168.0.106:3000"
+exports.APIUrl = process.env.REACT_APP_MYENV === "prod" ?  "https://test-i1dgsko38-maity54321s-projects.vercel.app/" : "https://test-i1dgsko38-maity54321s-projects.vercel.app/";
+// exports.devLink = "http://192.168.0.106:3000"// https://ecom-api-zdk9.onrender.com
