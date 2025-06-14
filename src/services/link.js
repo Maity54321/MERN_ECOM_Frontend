1908e8940b3518642acceff4e28966072aee8444
Maity54321 <maitysurodoy@gmail.com>
2025-06-14 23:58:13 +0530

    Change link.js

--- src/services/link.js
+++ src/services/link.js
@@ -1,3 +1,3 @@
 console.log(process.env.REACT_APP_MYENV)
-exports.APIUrl = process.env.REACT_APP_MYENV === "prod" ?  "https://ecom-api-zdk9.onrender.com" : "https://ecom-api-zdk9.onrender.com";
+exports.APIUrl = process.env.REACT_APP_MYENV === "prod" ?  "test-mern-back.vercel.app" : "https://ecom-api-zdk9.onrender.com";
 // exports.devLink = "http://192.168.0.106:3000"// https://ecom-api-zdk9.onrender.com
